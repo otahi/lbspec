711c6138a13dfde759070e5d300365c3afa5d669
OTA Hiroshi <ota_h@nifty.com>
2014-03-08 05:49:02 +0900

    deletes unnessesary spec

--- spec/lbspec_spec.rb
+++ spec/lbspec_spec.rb
@@ -4,8 +4,4 @@ describe Lbspec do
   it 'should have a version number' do
     Lbspec::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should eq(true)
-  end
 end
